5088918f9c5ec5596229724585c02b015a6a1e6f
ольга <olgakargan72@gmail.com>
2022-07-22 08:55:39 +0400

    доделала домашку 2 раз

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 import java.util.Arrays;
 
 
@@ -7,9 +6,6 @@ public class Main {
 
 
         System.out.println("task #1");
-//
-//  Объявите три массива:
-//     1. Целочисленный массив, заполненный тремя цифрами – 1, 2 и 3 – с помощью ключевого слова new.
         int[] Array = new int[3];
         Array[0] = 1;
         Array[1] = 2;
@@ -17,53 +13,56 @@ public class Main {
         System.out.println(Arrays.toString(Array));
 
         for (int i = Array.length - 1; i >= 0; i--) {
-            System.out.print(Array[i] + " ");
-        }
-        System.out.println();
-        System.out.println("task #4");
-        for (int i = 0, n = 1; i <Array.length; n = n + 1, i++) {
-            Array[i] = n+1;
-            System.out.print(Array[i] + " ");
+            System.out.print(Array[i] + ",");
+            System.out.print(Array[i]);
         }
         System.out.println();
 
+
         System.out.println("task #2");
-//     2. Массив, в котором можно хранить три дробных числа – 1.57, 7.654, 9.986 –  сразу заполнив его значениями
 
         double[] secondArray = {1.57, 7.654, 9.986};
         System.out.println(Arrays.toString(secondArray));
         for (int i = secondArray.length - 1; i >= 0; i--) {
-            System.out.print(secondArray[i] + " ");
+            System.out.print(secondArray[i] + ",");
+            System.out.print(secondArray[i]);
         }
         System.out.println();
+
         System.out.println("task #3");
-//     3. Произвольный массив – тип и количество данных определите сами.
-//     Самостоятельно выберите способ создания массива: с помощью ключевого слова или сразу
-//     заполненный элементами.v
+
         int[] Mas = new int[5];
         for (int i = 0, n = 1; i < Mas.length; n = n + 2, i++) {
             Mas[i] = n;
         }
         for (int i = Mas.length - 1; i >= 0; i--) {
-            System.out.print(Mas[i] + " ");
+            System.out.print(Mas[i] + ",");
+            System.out.print(Mas[i]);
         }
 
 
         System.out.println();
 
-//        Пройдитесь по первому целочисленному массиву и все нечетные числа в нем сделайте четными
-//        (нужно прибавить 1).
-//                Важно: код должен работать с любым целочисленным массивом,
-//                поэтому для решения задания вам нужно использовать циклы.
-//                Распечатайте результат преобразования в консоль.
+        System.out.println("task #4");
 
 
+        System.out.println();
+        int[] Arr = {1, 2, 3, 4, 5, 10, 11, 12, 13, 14};
 
+        int temp;
 
-
-
+        for (int j = 1; j < Arr.length; j += 2) {
+            temp = Arr[j - 1];
+            Arr[j - 1] = Arr[j];
+            Arr[j] = temp;
+        }
+        System.out.print(Arrays.toString(Arr));
     }
 }
+
+
+
+
 
 
 
